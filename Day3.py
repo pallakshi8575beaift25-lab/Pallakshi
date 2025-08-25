d287f1a443fc1af8e8b01b951759e0fbec766fdc
Pallakshi <pallakshi8575.beaift25@chitkara.edu.in>
2025-08-25 19:10:19 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,21 @@
+#loops in python : 
+for i in range (1,1000):
+    #print("Numbers :", i)
+
+
+ fruits = [ "cherry", "grapes", "mango"]
+print(fruits[0])
+print(fruits)
+for fruit in fruits:
+    print("I Like:", fruit)
+
+
+
+ #while condition 
+# code block 
+
+i=1
+while i<=5:
+    print("Numbers:",i)
+    i+=1
+
